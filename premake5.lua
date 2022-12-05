8c33afd0c09d69fc1f2105abcbc1657a8b226e45
Samuel Martel <martelsamuel00@gmail.com>
2022-12-05 10:49:47 -0500

    Fix premake

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,6 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
